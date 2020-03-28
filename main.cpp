78395e8a094afa7606910a30df5f513f83d15cc5
Bushfries <Bushfries@gmail.com>
2020-03-28 15:57:10 -0500

    I did some stuff there is a segfault

--- main.cpp
+++ main.cpp
@@ -7,272 +7,291 @@
 
 using namespace std;
 
-// Adds words from provided file to a dictionary
-void buildDict(Dictionary &d, string filename) {
-    fstream in;
-    in.open(filename);
-    string buf;
 
-    while(getline(in, buf)) {
-        if (!d.findEntry(buf)) {
-            d.addEntry(buf);
-        }
-    }
+// void split(vector<string> &words, vector<int> &lines, int line, string str) {
+//     string temp = "";
 
-    in.close();
-}
+//     for (int i = 0; i < str.length(); i++) {
+//         if (!isalpha(str[i]) && temp != "") {
+            
+//             words.push_back(temp);
+//             lines.push_back(line);
+//             temp = "";
+//         } else if (isalpha(str[i])) {
+//             temp += tolower(str.at(i));
+//         }
+//     }
+// }
 
+// void split(Dictionary &d, string str) {
 
-// C++ doens't have a split fucntion for some reason so I made this.
-// TODO: get line number in here somehow
-void split(vector<string> &words, vector<int> &lines, int line, string str) {
-    string temp = "";
-
-    for (int i = 0; i < str.length(); i++) {
-        if (!isalpha(str[i]) && temp != "") {
-            words.push_back(temp);
-            lines.push_back(line);
-            temp = "";
-        } else if (isalpha(str[i])) {
-            temp += tolower(str.at(i));
-        }
-    }
-}
+// }
 
 
-// Gets a vector of words from a provided file.
-void getWordVector(vector<string> &words, vector<int> &lines, string filename) {
-    fstream in;
-    in.open(filename);
-    string buf;
-    int line = 1;
-    while(getline(in, buf)) {
-        split(words, lines, line, buf);
-        line++;
-    }
+// // Gets a vector of words from a provided file.
+// void getWordVector(vector<string> &words, vector<int> &lines, string filename) {
+//     fstream in;
+//     in.open(filename);
+//     string buf;
+//     int line = 1;
+//     while(getline(in, buf)) {
+//         split(words, lines, line, buf);
+//         line++;
+//     }
 
-    in.close();
-}
-
-
-void addLetters(vector<string> &v, string &word) {
-    //for every position in word
-    for (int pos = 0; pos <= word.size(); pos++) {
-    //  add a-z
-        for (int let = 97; let <= 122; let++) {
-            string temp = word;
-            string curr = "";
-            curr += (char)let;
-            temp.insert(pos, curr);
-            v.push_back(temp);
-        }
-    }
-}
+//     in.close();
+// }
 
 
-void delLetters(vector<string> &v, string &word) {
-    for (int i = 0; i < word.size(); i++) {
-        string temp = word; 
-        temp.erase(i, 1);
-        v.push_back(temp);
-    }
-}
+// // Adds words from provided file to a dictionary
+// void buildDict(Dictionary &d, string filename) {
+//     fstream in;
+//     in.open(filename);
+//     string buf;
+//     vector<string> temp; 
+//     vector<int> _;
+
+//     while(getline(in, buf)) {
+//         split(temp, _, 0, buf);
+
+//         for (int i = 0; i < temp.size(); i++) {    
+//             if (!d.findEntry(temp.at(i))) {
+//                 d.addEntry(temp.at(i));
+//             }
+//         }
+        
+//         temp.clear();
+//     }
+
+//     in.close();
+// }
 
 
-void swapLetters(vector<string> &v, string &word) {
-    for (int i = 0; i < word.size() - 1; i++) {
-        for (int j = 0; j < word.size(); j++) {
-            string temp = word;
-            swap(temp[i], temp[j]);
-            v.push_back(temp);
-        }
-    }
-}
+// void addLetters(vector<string> &v, string &word) {
+//     //for every position in word
+//     for (int pos = 0; pos <= word.size(); pos++) {
+//     //  add a-z
+//         for (int let = 97; let <= 122; let++) {
+//             string temp = word;
+//             string curr = "";
+//             curr += (char)let;
+//             temp.insert(pos, curr);
+//             v.push_back(temp);
+//         }
+//     }
+// }
 
 
-void generateSuggestions(Dictionary &dict, vector<string> &v, string &word) {
-    vector<string> gen;
-
-    addLetters(gen, word);
-    delLetters(gen, word);
-    swapLetters(gen, word);
-
-    for (int i = 0; i < gen.size(); i++) {
-        if (dict.findEntry(gen.at(i))) {
-            v.push_back(gen.at(i));
-        }
-    }
-}
+// void delLetters(vector<string> &v, string &word) {
+//     for (int i = 0; i < word.size(); i++) {
+//         string temp = word; 
+//         temp.erase(i, 1);
+//         v.push_back(temp);
+//     }
+// }
 
 
-void loadNewFile(vector<string> &words, vector<int> &lines, Dictionary &dict, const string &filename) {
-    vector<string> tempw;
-    vector<int> templ;
-        getWordVector(tempw, templ, "test1.txt");
-
-    for (int i = 0; i < tempw.size(); i++) {
-        if (!dict.findEntry(tempw.at(i))) {
-            words.push_back(tempw.at(i));
-            lines.push_back(templ.at(i));
-        }
-    }
-}
-
-void displayTitle() {
-    if (system("CLS")) {
-        system("clear");
-    }
-
-    cout << "-------------------------------------------------------------------------------------" << endl;
-    cout << "/  ___|| ___ \\|  ___|| |    | |     /  __ \\| | | ||  ___|/  __ \\| | / /|  ___|| ___ \\ " << endl;
-    cout << "\\ `--. | |_/ /| |__  | |    | |     | /  \\/| |_| || |__  | /  \\/| |/ / | |__  | |_/ /" << endl;
-    cout << "`--. \\ |  __/ |  __| | |    | |     | |    |  _  ||  __| | |    |    \\ |  __| |    /" << endl;
-    cout << "/\\__/ /| |    | |___ | |____| |____ | \\__/\\| | | || |___ | \\__/\\| |\\  \\| |___ | |\\ \\" << endl;
-    cout << "\\____/ \\_|    \\____/ \\_____/\\_____/  \\____/\\_| |_/\\____/  \\____/\\_| \\_/\\____/ \\_| \\_|" << endl;
-    cout << "-------------------------------------------------------------------------------------" << endl;
-    cout << endl;
-}
+// void swapLetters(vector<string> &v, string &word) {
+//     for (int i = 0; i < word.size() - 1; i++) {
+//         for (int j = 0; j < word.size(); j++) {
+//             string temp = word;
+//             swap(temp[i], temp[j]);
+//             v.push_back(temp);
+//         }
+//     }
+// }
 
 
-int menu(bool dictLoaded, bool filesLoaded) {
-    displayTitle();
+// void generateSuggestions(Dictionary &dict, vector<string> &v, string &word) {
+//     vector<string> gen;
 
-    if (dictLoaded) {
-        cout << "1) Load Another Dictionary File" << endl;
-    } else {
-        cout << "1) Load Dictionary File" << endl;
-    }
+//     addLetters(gen, word);
+//     delLetters(gen, word);
+//     swapLetters(gen, word);
+
+//     for (int i = 0; i < gen.size(); i++) {
+//         if (dict.findEntry(gen.at(i))) {
+//             v.push_back(gen.at(i));
+//         }
+//     }
+// }
+
+
+// void loadNewFile(vector<string> &words, vector<int> &lines, Dictionary &dict, const string &filename) {
+//     vector<string> tempw;
+//     vector<int> templ;
+//     getWordVector(tempw, templ, "test1.txt");
+
+//     for (int i = 0; i < tempw.size(); i++) {
+//         if (!dict.findEntry(tempw.at(i))) {
+//             words.push_back(tempw.at(i));
+//             lines.push_back(templ.at(i));
+//         }
+//     }
+// }
+
+// void displayTitle() {
+//     if (system("CLS")) {
+//         system("clear");
+//     }
+
+//     cout << "-------------------------------------------------------------------------------------" << endl;
+//     cout << "/  ___|| ___ \\|  ___|| |    | |     /  __ \\| | | ||  ___|/  __ \\| | / /|  ___|| ___ \\ " << endl;
+//     cout << "\\ `--. | |_/ /| |__  | |    | |     | /  \\/| |_| || |__  | /  \\/| |/ / | |__  | |_/ /" << endl;
+//     cout << "`--. \\ |  __/ |  __| | |    | |     | |    |  _  ||  __| | |    |    \\ |  __| |    /" << endl;
+//     cout << "/\\__/ /| |    | |___ | |____| |____ | \\__/\\| | | || |___ | \\__/\\| |\\  \\| |___ | |\\ \\" << endl;
+//     cout << "\\____/ \\_|    \\____/ \\_____/\\_____/  \\____/\\_| |_/\\____/  \\____/\\_| \\_/\\____/ \\_| \\_|" << endl;
+//     cout << "-------------------------------------------------------------------------------------" << endl;
+//     cout << endl;
+// }
+
+
+// int menu(bool dictLoaded, bool filesLoaded) {
+//     displayTitle();
+
+//     if (dictLoaded) {
+//         cout << "1) Load Another Dictionary File" << endl;
+//     } else {
+//         cout << "1) Load Dictionary File" << endl;
+//     }
     
-    cout << "2) Load Text File to Check" << endl;
+//     cout << "2) Load Text File to Check" << endl;
 
-    if (filesLoaded) {
-        cout << "3) Show Spelling Mistakes" << endl;
-    } else {
-        cout << "3) --- LOCKED (Please Load Text File) ---" << endl;
-    }
+//     if (filesLoaded) {
+//         cout << "3) Show Spelling Mistakes" << endl;
+//     } else {
+//         cout << "3) --- LOCKED (Please Load Text File) ---" << endl;
+//     }
     
-    cout << "4) Exit" << endl;
-    cout << endl;
+//     cout << "4) Exit" << endl;
+//     cout << endl;
 
-    cout << "Enter Option: ";
-    int select;
-    cin >> select;
+//     cout << "Enter Option: ";
+//     int select;
+//     cin >> select;
 
-    return select;
-}
+//     return select;
+// }
 
 
-string getFileName(int n) {
-    displayTitle();
-    string filename;
+// string getFileName(int n) {
+//     displayTitle();
+//     string filename;
 
-    while (true) {
-        if (n == 1) {
-            cout << "Enter dictionary filename: ";
-        } else {
-            cout << "Enter text filename: ";
-        }
+//     while (true) {
+//         if (n == 1) {
+//             cout << "Enter dictionary filename: ";
+//         } else {
+//             cout << "Enter text filename: ";
+//         }
         
         
-        cin >> filename;
+//         cin >> filename;
 
-        ifstream f(filename.c_str());
+//         ifstream f(filename.c_str());
 
-        if (f.good()) {
-            break;
-        } else {
-            cout << "Filename Doesn't Exist!" << endl;
-            cout << endl;
-        }
-    }
+//         if (f.good()) {
+//             break;
+//         } else {
+//             cout << "Filename Doesn't Exist!" << endl;
+//             cout << endl;
+//         }
+//     }
 
-    return filename;
-}
+//     return filename;
+// }
 
 
-void spellingMistakesMenu(vector<string> &misspelled, vector<int> lines, Dictionary d) {
-    int selection = -1;
-    vector<string> temp;
+// void spellingMistakesMenu(vector<string> &misspelled, vector<int> lines, Dictionary d) {
+//     int selection = -1;
+//     vector<string> temp;
 
-    while (selection != 0) {
-        displayTitle();
+//     while (selection != 0) {
+//         displayTitle();
 
-        cout << "Words not found in dictionary: " << endl;
-        cout << endl << "Line#) Word\n\tSuggestions" << endl << endl;
+//         cout << "Words not found in dictionary: " << endl;
+//         cout << endl << "Line#) Word\n\tSuggestions" << endl << endl;
         
-        for (int i = 0; i < misspelled.size(); i++) {
-                cout << lines.at(i) << ") " << misspelled.at(i) << endl;
+//         for (int i = 0; i < misspelled.size(); i++) {
+//                 cout << lines.at(i) << ") " << misspelled.at(i) << endl;
                 
-                generateSuggestions(d, temp, misspelled.at(i));
-                cout << "\t";
+//                 generateSuggestions(d, temp, misspelled.at(i));
+//                 cout << "\t";
                 
-                if (temp.size() > 0) {
-                    for (int j = 0; j < temp.size(); j++) {
-                        cout << temp.at(j) << " ";
-                    }
-                    cout << endl << endl;
-                } else {
-                    cout << "\"No Suggestions\"" << endl << endl;
-                }
+//                 if (temp.size() > 0) {
+//                     for (int j = 0; j < temp.size(); j++) {
+//                         cout << temp.at(j) << " ";
+//                     }
+//                     cout << endl << endl;
+//                 } else {
+//                     cout << "\"No Suggestions\"" << endl << endl;
+//                 }
                 
-                temp.clear();
-        }
+//                 temp.clear();
+//         }
         
-        cout << "Enter '0' to Exit..." << endl;
-        cin >> selection;
-    }
-}
+//         cout << "Enter '0' to Exit..." << endl;
+//         cin >> selection;
+//     }
+// }
 
 
 int main() {
-    Dictionary dict;
-    vector<string> words;
-    vector<int> lines;
-    bool shouldExit = false;
-    bool fileLoaded = false;
-    bool dictLoaded = false;
+    // Dictionary dict;
+    // vector<string> words;
+    // vector<int> lines;
+    // bool shouldExit = false;
+    // bool fileLoaded = false;
+    // bool dictLoaded = false;
 
-    while (!shouldExit) {
-        int select = menu(dictLoaded, fileLoaded);
-        vector<string> tempw;
-        vector<int> templ;
+    // while (!shouldExit) {
+    //     int select = menu(dictLoaded, fileLoaded);
+    //     vector<string> tempw;
+    //     vector<int> templ;
         
-        switch (select) {
-            case 1:
-                buildDict(dict, getFileName(1));
-                dictLoaded = true;
-                break;
+    //     switch (select) {
+    //         case 1:
+    //             buildDict(dict, getFileName(1));
+    //             dictLoaded = true;
+    //             break;
 
-            case 2:
-                words.clear();
-                lines.clear();
-                getWordVector(words, lines, getFileName(2));
-                fileLoaded = true;
-                break;
+    //         case 2:
+    //             words.clear();
+    //             lines.clear();
+    //             getWordVector(words, lines, getFileName(2));
+    //             fileLoaded = true;
+    //             break;
 
-            case 3:
-                if (fileLoaded) {
-                    for (int i = 0; i < words.size(); i++) {
-                        if (!dict.findEntry(words.at(i))) {
-                            tempw.push_back(words.at(i));
-                            templ.push_back(lines.at(i));
-                        }
-                    }
-                    spellingMistakesMenu(tempw, templ, dict);
-                    break;
-                }
+    //         case 3:
+    //             if (fileLoaded) {
+    //                 for (int i = 0; i < words.size(); i++) {
+    //                     if (!dict.findEntry(words.at(i))) {
+    //                         tempw.push_back(words.at(i));
+    //                         templ.push_back(lines.at(i));
+    //                     }
+    //                 }
+    //                 spellingMistakesMenu(tempw, templ, dict);
+    //                 break;
+    //             }
 
 
-            case 4:
-                shouldExit = true;
+    //         case 4:
+    //             shouldExit = true;
 
-                if (system("CLS")) {
-                    system("clear");
-                }
+    //             if (system("CLS")) {
+    //                 system("clear");
+    //             }
 
-                break;
-            default:
-                break;
-        }
-    }
+    //             break;
+    //         default:
+    //             break;
+    //     }
+    // }
+
+    Dictionary d;
+    d.addEntry("a");
+    d.addEntry("a");
+    d.addEntry("a");
+    d.addEntry("a");
+    d.addEntry("a");
 }
